9aff45875e1f72549e7009729159894fe00cd6f6
Vincent Xayasak <xayasakvincent@gmail.com>
2023-04-02 15:25:54 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -8,28 +8,3 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
       });
     });
 }); 
-
-const form = document.querySelector('#contact-form');
-const nameInput = document.querySelector('#name-input');
-const emailInput = document.querySelector('#email-input');
-const subjectInput = document.querySelector('#subject-input');
-const messageInput = document.querySelector('#message-input');
-
-/* Contact */
-form.addEventListener('submit', (event) => {
-  event.preventDefault();
-  if (!nameInput.value || !emailInput.value || !subjectInput.value || !messageInput.value) {
-    alert('Please fill out all fields.');
-    return;
-  }
-  
-  const url = `https://example.com/sendMessage?name=${nameInput.value}&email=${emailInput.value}&subject=${subjectInput.value}&message=${messageInput.value}`;
-  fetch(url)
-    .then(() => {
-      alert('Message sent!');
-      form.reset();
-    })
-    .catch(() => {
-      alert('There was an error sending your message. Please try again later.');
-    });
-});
