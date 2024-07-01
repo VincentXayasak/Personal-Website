2bd998ff62fa7c91450969c86d30ac5756ff8c10
Vincent Xayasak <xayasakvincent@gmail.com>
2024-06-30 18:09:12 -0700

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,8 @@
+window.onload = function() {
+    document.body.addEventListener('click', function() {
+        var audio = document.getElementById('background-music');
+        audio.play();
+    });
+};
+
+//InProgressMedia/In_Progress_149bpm_VX.mp3
