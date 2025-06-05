e7c47933e51701077fe7b8e6a207f9e14cbbf0d4
Vincent Xayasak <xayasakvincent@gmail.com>
2025-06-05 00:17:33 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+/* Automatic Scrolling. */
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+  
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+        behavior: 'smooth'
+      });
+    });
+}); 
